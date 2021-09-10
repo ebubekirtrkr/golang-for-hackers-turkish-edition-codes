a4226def4eef710d4e960bb24959f641c381b7ad
Ebubekir Türker <35612408+ebubekirtrkr@users.noreply.github.com>
2021-09-10 23:20:20 +0800

    Update main.go

--- ders1/main.go
+++ ders1/main.go
@@ -1,6 +1,6 @@
 package main
 
-//go mod init "ders1" diyerek go.mod dosyasını test packagei olarak oluşturduk
+//go mod init "ders1" diyerek go.mod dosyasını ders1 package'i olarak oluşturduk
 //daha sonra examples klasöründe bulunan dosyalara aşağıdaki gibi import ederek ulaşabiliriz
 import "ders1/examples"
 
